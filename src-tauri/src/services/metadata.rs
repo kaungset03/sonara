2a8976a970d5ae814a16f9f7f36d05c8ea1a570b
Kaung Set <kset2299@gmail.com>
2026-06-25 21:53:28 +0630

    chore: fix ui inconsistencies

--- src-tauri/src/services/metadata.rs
+++ src-tauri/src/services/metadata.rs
@@ -1,18 +1,17 @@
-use std::{fs, path::Path};
+use std::path::Path;
 
 use lofty::{
     file::{AudioFile, TaggedFileExt},
-    picture::MimeType,
     tag::Accessor,
 };
 
 use crate::models::song::Song;
 
-fn sanitize_filename(name: &str) -> String {
-    name.chars()
-        .map(|c| if c.is_alphanumeric() { c } else { '_' })
-        .collect()
-}
+// fn sanitize_filename(name: &str) -> String {
+//     name.chars()
+//         .map(|c| if c.is_alphanumeric() { c } else { '_' })
+//         .collect()
+// }
 
 pub fn extract_metadata(path: &Path) -> Result<Song, String> {
     // Read the file and parse its format
@@ -48,33 +47,33 @@ pub fn extract_metadata(path: &Path) -> Result<Song, String> {
         .unwrap_or_else(|| "Unknown Album".to_string());
 
     // Extract album artwork path if available and save it to
-    let mut album_artwork_path: Option<String> = None;
+    // let mut album_artwork_path: Option<String> = None;
 
-    if let Some(tag) = tag {
-        if let Some(picture) = tag.pictures().first() {
-            let extension = match picture.mime_type() {
-                Some(MimeType::Png) => "png",
-                Some(MimeType::Jpeg) => "jpg",
-                Some(MimeType::Bmp) => "bmp",
-                Some(MimeType::Gif) => "gif",
-                _ => "jpg",
-            };
+    // if let Some(tag) = tag {
+    //     if let Some(picture) = tag.pictures().first() {
+    //         let extension = match picture.mime_type() {
+    //             Some(MimeType::Png) => "png",
+    //             Some(MimeType::Jpeg) => "jpg",
+    //             Some(MimeType::Bmp) => "bmp",
+    //             Some(MimeType::Gif) => "gif",
+    //             _ => "jpg",
+    //         };
 
-            let file_name = format!(
-                "{}_{}.{}",
-                sanitize_filename(&album),
-                sanitize_filename(&artist),
-                extension
-            );
+    //         let file_name = format!(
+    //             "{}_{}.{}",
+    //             sanitize_filename(&album),
+    //             sanitize_filename(&artist),
+    //             extension
+    //         );
 
-            let full_path = Path::new("album_artwork").join(file_name);
+    //         let full_path = Path::new("album_artwork").join(file_name);
 
-            // write image to disk
-            fs::write(&full_path, &picture.data()).map_err(|e| e.to_string())?;
+    //         // write image to disk
+    //         fs::write(&full_path, &picture.data()).map_err(|e| e.to_string())?;
 
-            album_artwork_path = Some(full_path.to_string_lossy().into_owned());
-        }
-    }
+    //         album_artwork_path = Some(full_path.to_string_lossy().into_owned());
+    //     }
+    // }
 
     // Build and return a single Song instance
     Ok(Song {
@@ -82,7 +81,7 @@ pub fn extract_metadata(path: &Path) -> Result<Song, String> {
         title,
         artist,
         album,
-        album_artwork_path,
+        album_artwork_path: None,
         duration,
         path: path.to_string_lossy().into_owned(),
         lyrics_path: None,
